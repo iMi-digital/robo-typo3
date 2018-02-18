22a72f2317d622da17d400f9aa22623f17fcf36e
Alexander Menk <a.menk@imi.de>
2018-02-18 17:49:08 +0100

    :sparkles: :construction: Add typo3dbDevelopmentDump;  Table definitions, load config
    - Development table definition added
    - Load config might not work if typo3 code is in AdditionalConfiguration

--- src/Task/Typo3/loadShortcuts.php
+++ src/Task/Typo3/loadShortcuts.php
@@ -1,15 +1,33 @@
 <?php
+
 namespace iMi\RoboTypo3\Task\Typo3;
 
-trait loadShortcuts
-{
+trait loadShortcuts {
     /**
      * @param string $url
      *
      * @return \Robo\Result
      */
-    protected function _typo3cms($action)
-    {
-        return $this->taskTypo3Stack()->exec($action)->run();
+    protected function _typo3cms( $action ) {
+        return $this->taskTypo3Stack()->exec( $action )->run();
+    }
+
+    protected function _typo3getTableDefinitions() {
+        return [
+            'development' => [ 'tables' => [ 'cache cache_tag be_sessions sys_log cf_cache*' ] ],
+        ];
+    }
+
+    protected function _typo3dbDevelopmentDump( $filename ) {
+        $this->taskDatabaseStack( [
+            'dbname'   => 'club_sata_com',
+            'username' => 'root',
+            'password' => 'root',
+            'host'     => 'localhost'
+        ], $this->_typo3getTableDefinitions() )->setFilename( $filename )
+             ->setIsHumanReadable( true )
+             ->setAddTime( 'no' )
+             ->setStrip( '@development' )
+             ->dump()->run();
     }
 }
